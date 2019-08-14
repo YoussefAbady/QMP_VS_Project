62d03e6df87f8d197c84d73cd6b324f964dbfdc5
yyahiya <yyahiya@CAI1-L13307>
2019-08-15 00:48:06 +0200

    - Removing Excel pakage include to build project as there is no excel - Edit gitignore file

--- Resala_HR/Excel_Client.cs
+++ Resala_HR/Excel_Client.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using excel = Microsoft.Office.Interop.Excel;
+//using excel = Microsoft.Office.Interop.Excel;
 
 namespace Resala_HR
 {
